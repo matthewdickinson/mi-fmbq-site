a99ae400f17f9d8a3e2d3ac94618d06c1d1b229d
Matthew Dickinson <matt@codifi.us>
2024-08-30 00:48:54 -0400

    Added markdown content to homepage

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it")();
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 
@@ -19,6 +20,9 @@ module.exports = function (eleventyConfig) {
 
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  // Allow rendering of HTML
+  eleventyConfig.addFilter("markdown", value => markdownIt.render(value));
 
   // To Support .yaml Extension in _data
   // You may remove this if you can use JSON
